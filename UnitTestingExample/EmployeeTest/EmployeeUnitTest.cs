f4ff5200cdb9d040696d1574e3646dcc3fce46dc
Shaikh Irshad <shaikhij2000@gmail.com>
2021-04-08 12:20:18 +0530

    testing updates

--- UnitTestingExample/EmployeeTest/EmployeeUnitTest.cs
+++ UnitTestingExample/EmployeeTest/EmployeeUnitTest.cs
@@ -1,4 +1,10 @@
+using Moq;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnitTestingExample.Data.POCO;
+using UnitTestingExample.Repository.Interfaces;
 using Xunit;
 
 namespace EmployeeTest
@@ -8,7 +14,33 @@ namespace EmployeeTest
         [Fact]
         public void GetEmployeeById()
         {
+            //mock instance the repo
+            var service = new Mock<IEmployeeRepository>();
 
+            //create dummy data
+            var dummyData = new List<Employee>() {
+                new Employee()
+                {
+                     EmployeeName = "Test",
+                     Department = "Testing",
+                     EmployeeId = 1
+                },
+                new Employee()
+                {
+                     EmployeeName = "Test 2",
+                     Department = "Testing 2",
+                     EmployeeId = 2
+                }
+            };
+
+            //setup the dummy data for particular function we want to check
+            service.Setup(x => x.GetEmployeeById(2)).ReturnsAsync(dummyData.Where(x => x.EmployeeId == 2).ToList());
+
+            //count the retured result
+            var count = service.Object.GetEmployeeById(2).Result.Count();
+
+            //check expected and actual
+            Assert.Equal(1, count);
         }
     }
 }
--- UnitTestingExample/UnitTestingExample/Program.cs
+++ UnitTestingExample/UnitTestingExample/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UnitTestingExample.Data;
 
 namespace UnitTestingExample
 {
@@ -13,7 +16,39 @@ namespace UnitTestingExample
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<ApplicationDbContext>();
+                    context.Database.Migrate();
+
+                    if (context.Employees.Any())
+                    {
+                        context.Employees.Add(new Data.POCO.Employee
+                        {
+                            EmployeeName = "Test",
+                            Department = "Testing"
+                        });
+                        context.Employees.Add(new Data.POCO.Employee
+                        {
+                            EmployeeName = "Test 2",
+                            Department = "Testing 2"
+                        });
+                        context.SaveChanges();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
