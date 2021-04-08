b2d36f834a96081dfcc6877a1e681f1b4171c01a
Shaikh Irshad <shaikhij2000@gmail.com>
2021-04-08 08:32:44 +0530

    unit test project added

---
+++ UnitTestingExample/EmployeeTest/EmployeeUnitTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace EmployeeTest
+{
+    public class EmployeeUnitTest
+    {
+        [Fact]
+        public void GetEmployeeById()
+        {
+
+        }
+    }
+}
